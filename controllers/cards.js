50939f2c62ec63d2d4ec791dd26b64fc62c2fd55
Three Worlds <harrymidas@yandex.ru>
2022-12-21 22:40:30 +0300

    fix: catch errors of likes

--- controllers/cards.js
+++ controllers/cards.js
@@ -42,33 +42,42 @@ export const deleteCardById = async (req, res) => {
 };
 
 export const setLikeByCardId = async (req, res) => {
-  try {
-    await CardModel.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-      { new: true },
-    ).populate('likes').then((responce) => res.send({ data: responce }));
-  } catch (err) {
-    if (err.name === 'NotFound') {
-      res.status(404).send({ message: 'Карточка с указанным id не найдена' });
-    } else {
-      res.status(500).send({ message: `Произошла ошибка: ${err.name} текст ошибки: ${err.message}` });
-    }
-  }
+  await CardModel.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  ).populate('likes')
+    .orFail(new Error('NotFound'))
+    .then((responce) => res.send({ data: responce }))
+    .catch((err) => {
+      /* console.log(`ВОТ ТУТ!!! ${err}`); */
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+      } else if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка с указанным id не найдена' });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка: ${err.name} текст ошибки: ${err.message}` });
+      }
+    });
 };
 
 export const unsetLikeByCardId = async (req, res) => {
-  try {
-    await CardModel.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } }, // убрать _id из массива
-      { new: true },
-    ).populate('likes').then((responce) => res.send({ data: responce }));
-  } catch (err) {
-    if (err.name === 'NotFound') {
-      res.status(404).send({ message: 'Карточка с указанным id не найдена' });
-    } else {
-      res.status(500).send({ message: `Произошла ошибка: ${err.name} текст ошибки: ${err.message}` });
-    }
-  }
+  await CardModel.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .populate('likes')
+    .orFail(new Error('NotFound'))
+    .then((responce) => res.send({ data: responce }))
+    .catch((err) => {
+      /* console.log(`ВОТ ТУТ!!! ${err}`); */
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+      } else if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка с указанным id не найдена' });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка: ${err.name} текст ошибки: ${err.message}` });
+      }
+    });
 };
